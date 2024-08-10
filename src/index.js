705699738b42510ab80b8b1f07d72bc79665d22d
makris charalampos <mcshooter23@gmail.com>
2024-08-10 14:12:31 +0300

    Improve Output Presentation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import chalk from "chalk";
 import { generateQuote } from "./generateQuote.js";
 
 // Generate and display a random quote to the user
 const randomQuote = generateQuote();
-console.log("Your Inspirational Quote:");
-console.log(randomQuote);
+
+// Apply chalk styling with additional formatting
+console.log(chalk.bgCyan.black.bold("=== Inspirational Quote ==="));
+console.log(chalk.green.bold(randomQuote));
+console.log(chalk.bgCyan.black("================================="));
